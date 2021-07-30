d46591a4370f148e275d85a8b02632a85f36129e
olgashumilova <volga_shumilova@mail.ru>
2021-07-30 17:34:25 +0300

    feat: add signin, signup and pass validation

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors')
-const multer  = require('multer')
 const bodyParser = require('body-parser')
-const app = express();
-const arrayOfGames = require('./games.json');
+const bcrypt = require('bcrypt');
+const app = express()
+const arrayOfGames = require('./games.json')
+const arrayOfUsers = []
 
 app.use(
   express.json({
@@ -13,6 +14,12 @@ app.use(
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+
+// Sign up check
+let isSignedUp = false
+
+// Password Hash
+let signUpHash
 
 const PORT = 3001
 app.listen(PORT, () => {
@@ -26,38 +33,52 @@ app.get(`/search/:text`, (req, res) => {
     const res = game.name.toLowerCase().includes(userText);
     return res;
   })
-
   res.send(arrOfNames)
   res.sendStatus = 200
   if (!req.body) return res.sendStatus(400);
 })
 
-app.get('/getTopGames', (_req, res) => {
+app.get('/getTopGames', (req, res) => {
   let topGames = []
   let count = 0;
 
   for (let i = 0; i < arrayOfGames.length; i++) {
-
     if (arrayOfGames[i].rating === 5) {
       count++
       topGames.push(arrayOfGames[i])
-
       if (count > 3) break
     }
   }
-
   res.send(topGames)
   res.sendStatus = 200
   if (!req.body) return res.sendStatus(400);
 })
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
   let {login, password} = req.body
+  const correctPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g.test(password)
+  const salt = await bcrypt.genSalt(10)
+  signUpHash = await bcrypt.hash(password, salt)
+
   if (!login && !password) {
     res.send('Enter data')
   } else if (login && password) {
-    res.send('You\'ve been signed up!')
-    res.sendStatus(200)
+
+    for (let i = 0; i < arrayOfUsers.length; i++) {
+      if (login === arrayOfUsers[i].login) {
+        res.send('This login has already taken')
+      }
+    }
+    
+    if (!correctPass) {
+      res.send('Password must contain minimum eight characters, at least one letter and one number')
+    } else if (correctPass) {
+      isSignedUp = true
+      arrayOfUsers.push({login: login, isSignedUp: isSignedUp, password: signUpHash})
+      res.send('You\'ve been signed up!')
+      res.sendStatus(200)
+    }
+
   } else if (!login) {
     res.send('Enter login')
   } else if (!password) {
@@ -66,17 +87,46 @@ app.post('/signup', (req, res) => {
   if (!req.body) return res.sendStatus(400);
 })
 
-app.post(`/signin`, (req, res) => {
+app.post(`/signin`, async (req, res) => {
   let {login, password} = req.body
-  if (!login && !password) {
-    res.send('Enter data')
-  } else if (login && password) {
-    res.send('Welcome!')
-    res.sendStatus(200)
-  } else if (!login) {
-    res.send('Enter login')
-  } else if (!password) {
-    res.send('Enter password')
+  const validPassword = await bcrypt.compare(password, signUpHash);
+
+  try {
+    if (isSignedUp === true) {
+    
+      if (!login && !password) {
+        res.send('Enter data')
+      } else if (login && password) {
+    
+        for (let i = 0; i < arrayOfUsers.length; i++) {
+          if (arrayOfUsers[i].login !== login) {
+            res.send('This user doesn\'t exist')
+          } else if (!validPassword) {
+            res.send('Enter valid password')
+          } else if (validPassword) {
+            res.send(`Welcome, ${arrayOfUsers[i].login}!`)
+            res.sendStatus(200)
+          }
+        }
+    
+      } else if (!login) {
+        res.send('Enter login')
+      } else if (!password) {
+        res.send('Enter password')
+      }
+    } else {
+      res.send('Sign up first')
+    }
+  } catch (error) {
+    res.send(error)
   }
+
+
+  if (!req.body) return res.sendStatus(400);
+})
+
+app.get('/getUsersArray', (_req, res) => {
+  res.send(arrayOfUsers)
+  res.sendStatus = 200
   if (!req.body) return res.sendStatus(400);
 })
